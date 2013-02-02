3689dfc0b5bfe8665bced658f74ea0d2ecec5426
Rath1on <ContagionMP@gmail.com>
2013-02-02 00:27:10 -0500

    module and zImage copy

---
+++ getModules.sh
@@ -0,0 +1,12 @@
+cp drivers/crypto/msm/qce40.ko gotModules/
+cp drivers/crypto/msm/qcedev.ko gotModules/
+cp drivers/crypto/msm/qcrypto.ko gotModules/
+cp drivers/input/evbug.ko gotModules/
+cp drivers/interceptor/vpnclient.ko gotModules/
+cp drivers/media/video/gspca/gspca_main.ko gotModules/
+cp drivers/net/wireless/bcmdhd/dhd.ko gotModules/
+cp drivers/net/wireless/btlock/btlock.ko gotModules/
+cp drivers/scsi/scsi_wait_scan.ko gotModules/
+cp drivers/spi/spidev.ko gotModules/
+cp arch/arm/boot/zImage gotModules/
+
